9a47b10ee38d26d84080c18ab895b7af83f3cb38
Milena Tyagra <milenatyagra.sc@gmail.com>
2023-11-05 15:48:30 -0300

    Criação de componente Home

--- src/components/layout/content/Content.jsx
+++ src/components/layout/content/Content.jsx
@@ -2,11 +2,15 @@ import "./Content.css";
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import NotFound from '../../../views/examples/NotFound'
+import Home from "../../../views/examples/Home";
+import NotFound from "../../../views/examples/NotFound";
 
 const Content = () => (
   <main className="Content">
     <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
       <Route path="*">
         <NotFound />
       </Route>
---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PageTitle from "../../components/layout/pageTitle/PageTitle";
+
+const Home = () => (
+  <div className="Home">
+    <PageTitle
+      title="Módulo Hooks"
+      subtitle="Hooks são uma nova adição ao React 16.8."
+    />
+  </div>
+);
+
+export default Home;
