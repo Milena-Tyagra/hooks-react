1d8f7bc06c49da4254b37bae8cd175eecb8c7f30
Milena Tyagra <milenatyagra.sc@gmail.com>
2023-11-05 15:44:19 -0300

    Criação de componente NotFounded

---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PageTitle from "../../components/layout/pageTitle/PageTitle";
+
+const Home = () => (
+  <div className="Home">
+    <PageTitle error title="404" subtitle="Opss... Página Não Encontrada!" />
+  </div>
+);
+
+export default Home;
